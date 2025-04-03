061f22854a20c324ad5243eb3dc5c615cedfc2a6
Fallourd Matthieu <fallourd.matthieu@gmail.com>
2025-04-03 13:58:51 +0200

    fix: load images

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -17,17 +17,20 @@ const Projects = () => {
         <div>
             <h3 className="text-center title">PROJECTS</h3>
             <div className="flex justify-content-evenly wrap">
-                {projects.map((item, i) => (
+                {projects.map((item, i) => {
+                  const imgPath = new URL(`../../assets/img/${item.source}`, import.meta.url).href;
+                  return (
                     <a key={i} href={item.link} target="_blank" rel="noreferrer" className="link-project">
                         <div className="card">
                             <div className="projects_img_div">
-                                <img className="projects_img" src={`/img/${item.source}`} alt={item.desc_img} />
+                                <img className="projects_img" src={imgPath} alt={item.desc_img} />
                             </div>
                             <p className="projects_name">{item.name}</p>
                             <p className="projects_desc">{item.description}</p>
                         </div>
                     </a>
-                ))}
+                  )
+                })}
             </div>
         </div>
     );
